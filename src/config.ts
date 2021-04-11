2307c2506fc754b8c8d4ba7a274f66d4df558d1a
Yaroslav <kk2031@mail.ru>
2021-04-11 20:47:57 +0300

    build(configs): Изменены конфиги Изменен конфиг для работы сервера и базы данных, добавлен пример конфига nginx

--- src/config.ts
+++ src/config.ts
@@ -21,6 +21,8 @@ const config = {
 	database: {
 		host: process.env.DB_HOST,
 		port: process.env.DB_PORT,
+		name: process.env.DB_NAME,
+		username: process.env.DB_USER,
 		password: process.env.DB_PASSWORD
 	}
 };
