54c6d1d2fc2459e3805760ede3da41a849072546
Yaroslav <kk2031@mail.ru>
2021-04-12 01:15:31 +0300

    feat(configs) Добавлен валидатор для .env

--- src/config.ts
+++ src/config.ts
@@ -5,25 +5,27 @@
 
 import dotenv from "dotenv";
 import * as path from "path";
+import {validateEnv} from "./utils/validateEnv";
 
 dotenv.config({
 	path: path.join(__dirname, ".env")
 });
 
+const env = validateEnv(process.env);
+
 const config = {
 	server: {
 		host: "localhost",
-		port: Number(process.env.SERVER_PORT),
-		jwtSecret: process.env.JWT_SECRET || "8864f61af0e543d3aa83cc2113a98ef9",
-		jwtRefreshSecret:
-			process.env.JWT_REFRESH_SECRET || "d53cc6a3ae314479922d8aa8a79bffd7"
+		port: env.SERVER_PORT,
+		jwtSecret: env.JWT_SECRET,
+		jwtRefreshSecret: env.JWT_REFRESH_SECRET
 	},
 	database: {
-		host: process.env.DB_HOST,
-		port: process.env.DB_PORT,
-		name: process.env.DB_NAME,
-		username: process.env.DB_USER,
-		password: process.env.DB_PASSWORD
+		host: env.DB_HOST,
+		port: env.DB_PORT,
+		name: env.DB_NAME,
+		username: env.DB_USER,
+		password: env.DB_PASSWORD
 	}
 };
 
---
+++ src/utils/validateEnv.ts
@@ -0,0 +1,16 @@
+import {
+	cleanEnv, str, port
+} from 'envalid';
+
+export function validateEnv(env: unknown) {
+	return cleanEnv(env, {
+		SERVER_PORT: port({desc: "Порт веб-сервера, по которому он \"слушает\" запросы"}),
+		DB_HOST: str({desc: "Хост базы данных"}),
+		DB_PORT: port({desc: "Порт базы данных"}),
+		DB_NAME: str({desc: "Имя базы данных"}),
+		DB_USER: str({desc: "Имя пользователя базы данных"}),
+		DB_PASSWORD: str({desc: "Пароль от пользователя базы данных"}),
+		JWT_SECRET: str({ default: '7855b9307fa24bb785f2deaa9c702930' }),
+		JWT_REFRESH_SECRET: str({ default: '04bbed8fb6ed42c9815a5ecf8eb9b10c' })
+	});
+}
